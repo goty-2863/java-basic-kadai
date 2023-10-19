4459d14664e66ca4aba383f30a8bbc4f18a61075
goty-2863 <146706046+goty-2863@users.noreply.github.com>
2023-10-19 17:08:43 +0900

    kadai_020をアップロード

---
+++ kadai_20/DictionaryExec_Chapter21.java
@@ -0,0 +1,16 @@
+package kadai_20;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		//Map<String, String> dictionaly = new HashMap<String, String>();
+		Dictionary_Chapter20 dictionaly = new Dictionary_Chapter20();
+
+		String[] words = { "apple", "banana", "grape", "orange" };
+
+		dictionaly.reference(words);
+	}
+}
+
+//for (int i = 0; i < word.length; i++) {
+//if (word[i].equals(dictionaly.key)) {
